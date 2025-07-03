79e3433cec6dbbfcaf89da413a90686d5ce83e3a
Fifixex <fifix_dev@proton.me>
2025-07-03 01:14:24 +0000

    feat: add acronym guild icon

--- apps/frontend/src/components/cards/GuildInfo.tsx
+++ apps/frontend/src/components/cards/GuildInfo.tsx
@@ -6,19 +6,26 @@ import { Users } from 'lucide-react';
 export interface GuildInfoProps {
   guild: Guild;
   owner: GuildMember;
+  invite: string;
 }
 
-export const GuildInfo = ({ guild, owner }: GuildInfoProps) => {
+export const GuildInfo = ({ guild, owner, invite }: GuildInfoProps) => {
   return (
     <Card className="w-full max-w-md mx-auto bg-background">
       <CardHeader className="pb-4">
         <div className="flex items-center gap-3">
           <div className="relative">
-            <img
-              src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png?size=2048`}
-              alt={guild.name}
-              className="w-12 h-12 rounded-full"
-            />
+            {guild.icon ? (
+              <img
+                src={`https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.png?size=2048`}
+                alt={guild.name}
+                className="w-12 h-12 rounded-full"
+              />
+            ) : (
+              <span className="w-12 h-12 p-4 rounded-full bg-accent-foreground text-background">
+                {guild.nameAcronym}
+              </span>
+            )}
             <div className="absolute -bottom-1 -right-1 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></div>
           </div>
           <div className="flex-1">
@@ -40,7 +47,9 @@ export const GuildInfo = ({ guild, owner }: GuildInfoProps) => {
         <hr />
 
         <div className="flex gap-2 pt-2">
-          <Button className="flex-1">Join the server</Button>
+          <a href={invite} target="_blank" className="w-full">
+            <Button className="flex-1 cursor-pointer">Join the server</Button>
+          </a>
         </div>
       </CardContent>
     </Card>
