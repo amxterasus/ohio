402440ee9fc58b11dc30ba840da906f90ff7c7f3
Fifixex <fifix_dev@proton.me>
2025-07-03 01:13:46 +0000

    feat: add create invite for random text channel

--- apps/backend/src/routes/guilds.ts
+++ apps/backend/src/routes/guilds.ts
@@ -1,3 +1,4 @@
+import { ChannelType } from 'discord.js';
 import { Hono } from 'hono';
 import { client } from '../client';
 
@@ -14,11 +15,17 @@ guildRouter.get('/:id', async (c) => {
   const guild = client.guilds.cache.get(guildId);
 
   const owner = await guild?.fetchOwner({ force: true });
+  const channel = guild?.channels.cache.find(
+    (c) => c.type === ChannelType.GuildText,
+  );
+  const invite = channel
+    ? (await channel.createInvite()).url
+    : `https://discord.gg/${guild?.vanityURLCode}`;
 
   if (!guild || !owner)
     return c.json(
       { status: false, message: 'not found server or owner!' },
       500,
     );
-  return c.json({ guild, owner });
+  return c.json({ guild, owner, invite });
 });
