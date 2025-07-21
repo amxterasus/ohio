69a3e3bd9afd6616f96ddd01ce24291a9dbd6d11
amxterasus <amxterasus@gmail.com>
2025-07-22 01:11:16 +0200

    feat: vercel configs

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -2,11 +2,13 @@
 
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import vercel from "@astrojs/vercel";
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },
